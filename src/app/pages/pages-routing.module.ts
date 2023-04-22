e58ba297aa7862622e51ef55486cfa0752f5d3e3
Anhvndev <anhvndev@gmail.com>
2023-04-23 00:06:38 +0700

    create fee management

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { PagesComponent } from './pages.component';
+import { ManagementFeeComponent } from './management-fee/management-fee.component';
+
 
 const routes: Routes = [
   {
@@ -14,6 +16,11 @@ const routes: Routes = [
       },
 
       //Implement component other
+
+      {
+        path:'managementFee',
+        component: ManagementFeeComponent
+      }
     ],
   },
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Ng-Zorro
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { BasicLayoutModule } from '../layouts/basic-layout/basic-layout.module';
 import { TestComponent } from './test/test.component';
+import { ManagementFeeComponent } from './management-fee/management-fee.component';
 
 @NgModule({
   declarations: [
     PagesComponent,
     TestComponent,
+    ManagementFeeComponent,
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
     BasicLayoutModule,
+    NzCardModule,
+    NzDropDownModule,
+    NzModalModule,
+    NzDatePickerModule
   ]
 })
 export class PagesModule { }
