2354bf09db4680c0e3e1e1f2b572101085507681
Manfred11 <manhcuong.qn1998@gmail.com>
2023-04-25 23:59:35 +0700

    implement auth-service, login screen

--- src/app/layouts/basic-layout/basic-layout.module.ts
+++ src/app/layouts/basic-layout/basic-layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicLayoutComponent } from './basic-layout.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+// import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { IconDefinition } from '@ant-design/icons-angular';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import * as AllIcons from '@ant-design/icons-angular/icons';
@@ -19,7 +19,6 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesign
   imports: [
     CommonModule,
     NzLayoutModule,
-    NzBreadCrumbModule,
     NzMenuModule,
     NzIconModule.forChild(icons),
     NzDropDownModule,
